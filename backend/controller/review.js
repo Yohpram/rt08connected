952a1697285daa755c3e9218d90d0b01995465ad
Yohanes Walwiyo Pramuviadi <126755927+Yohpram@users.noreply.github.com>
2024-09-18 17:45:09 +0700

    Update review.js

--- backend/controller/review.js
+++ backend/controller/review.js
@@ -1,44 +1,46 @@
 const ReviewModel = require('../models/review');
-const { decodeToken  } = require('../middleware/auth');
+const { decodeToken } = require('../middleware/auth');
 
 const ReviewController = {
-  async getReviewsByProductId(req, res) {
-    const productId = req.params.id;
+  async getAllReviews(req, res) {
     try {
-      const reviews = await ReviewModel.getReviewsByProductId(productId);
+      const reviews = await ReviewModel.getAllReviews();
       res.json(reviews);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   },
 
-  async addReviewByProductId(req, res) {
+  async addReview(req, res) {
     const { review } = req.body;
-    const productId = req.params.id;
 
-    if (!review || !productId) {
-      return res.status(400).json({ error: 'Harap lengkapi semua bidang ulasan.' });
+    if (!review) {
+      return res.status(400).json({ error: 'Harap lengkapi bidang ulasan.' });
     }
 
     try {
       const token = req.header('x-auth-token');
-      
+
       if (!token) {
-        return res.status(401).json({ error: 'Access denied. No token provided.' });
+        return res.status(401).json({ error: 'Akses ditolak. Token tidak ditemukan.' });
       }
 
       const { id: userId } = decodeToken(token);
 
-      const newReview = await ReviewModel.addReviewByProductId(review, productId, userId);
+      const newReview = await ReviewModel.addReview(review, userId);
       res.status(201).json({ message: 'Ulasan berhasil ditambahkan.', review: newReview });
     } catch (error) {
       res.status(500).json({ error: error.message });
-    } 
-  },     
+    }
+  },
 
   async updateReview(req, res) {
     const reviewId = req.params.reviewId;
-    const newReview = req.body.review;
+    const { review: newReview } = req.body;
+
+    if (!newReview) {
+      return res.status(400).json({ error: 'Harap masukkan ulasan yang diperbarui.' });
+    }
 
     try {
       const updatedReview = await ReviewModel.updateReviewById(reviewId, newReview);
